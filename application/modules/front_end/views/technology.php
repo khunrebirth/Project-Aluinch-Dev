a5d9d7a0a36ab1433c9ca5a95346f661257866f9
khunrebirth <khunrebirth@gmail.com>
2019-08-21 17:50:55 +0700

    update: project

--- application/modules/front_end/views/technology.php
+++ application/modules/front_end/views/technology.php
@@ -1,5 +1,3 @@
-    <!-- CSS Libraries -->
-
     <!-- Push Custom CSS -->
     <style>
         .main-section ul.list-item>li:before {
@@ -109,130 +107,12 @@
     </section>
 
     <!-- contact -->
-    <script src='https://www.google.com/recaptcha/api.js?hl=th'></script>
-    <script>
-        function check-captcha(){
-            document.getElementById('btn-footer-submit').disabled = false;
-        }
-    </script>
-    <style type="text/css">
-        button[disabled], html input[disabled] {
-            opacity: 0.5;
-        }
-    </style>
-    <div id="contact">
-        <div class="graphic"><img src="<?php echo base_url('storage/images/product/graphic-footer.png'); ?>" class="img-responsive" /></div>
-        <div class="main-box-head">
-            <div class="warp">
-                <div class="box-header">CONTACT<br />US</div>
-            </div>
-        </div>
-        <div class="bg"></div>
-        <div class="bg-orange">
-            <div class="warp">
-                <div class="box-contact box-contact-1">
-                    <div class="clearfix"></div>
-                    <span class="head">CONTACT INFO</span>
-                    <div class="clearfix"></div>
-                    <div class="list-contact">
-                        <div class="media">
-                            <div class="media-left media-top">
-                                <img class="media-object" src="<?php echo base_url('storage/images/c1.png'); ?>">
-                            </div>
-                            <div class="media-body">
-                                Alumination Ltd.<br />
-                                1369 Room No. 7, Chan road, Thungwatdon, Sathorn, Bangkok 10120<br />
-                                Fax : 02 286 3669
-                            </div>
-                        </div>
-                        <div class="media box-c">
-                            <div class="media-left media-middle">
-                                <img class="media-object" src="<?php echo base_url('storage/images/c3.png'); ?>">
-                            </div>
-                            <div class="media-body" style="padding-top:5px;">
-                                <a href="mailto:info@aluinch.com">info@aluinch.com</a>
-                            </div>
-                        </div>
-                        <div class="contact-clear"></div>
-                        <div class="media box-c">
-                            <div class="media-left media-middle">
-                                <img class="media-object" src="<?php echo base_url('storage/images/c5.png'); ?>">
-                            </div>
-                            <div class="media-body map" style="padding-top:5px;" data-toggle="modal" data-target="#map">
-                                VIEW COMPANY MAP
-                            </div>
-                        </div>
-                    </div>
-                    <div class="list-contact2">
-                        <div class="media box-c">
-                            <div class="media-left media-middle">
-                                <img class="media-object" src="<?php echo base_url('storage/images/c2.png'); ?>">
-                            </div>
-                            <div class="media-body" style="padding-top:5px;">
-                                <a href="tel:022863666"> 02 286 3666</a>
-                            </div>
-                        </div>
-                        <div class="media box-c">
-                            <div class="media-left media-middle">
-                                <img class="media-object" src="<?php echo base_url('storage/images/c4.png'); ?>">
-                            </div>
-                            <div class="media-body" style="padding-top:5px;">
-                                www.aluinch.com
-                            </div>
-                        </div>
-                        <div class="media box-c desktop">
-                            <div class="media-body">
-                                <br /> Sitemap
-                                <br />Terms and Conditions
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="clearfix"></div>
-                    <div class="copy desktop">Copyright © 2015 www.aluinch.com All Rights Reserved. SEO maintained by iSEO.in.th</div>
-                    <div class="copy-line desktop"></div>
-                    <div class="partner desktop">partnership by <img src="<?php echo base_url('storage/images/partner-logo.jpg'); ?>" width="100" /> Company</div>
-                </div>
-                <div class="box-contact form-contact">
-                    <form method="post" action="https://www.aluinch.com/sendemail_system.php">
-                        <input type="text" name="name" placeholder="Name:" required />
-                        <input type="text" name="email" placeholder="Email:" required />
-                        <input type="text" name="company" placeholder="Company:" />
-                        <input type="text" name="tel" placeholder="Phone:" />
-                        <textarea name="detail" placeholder="Message:" required ></textarea>
-                        <div style="padding-bottom: 20px;" data-callback="check-captcha" class="g-recaptcha" data-sitekey="6LcVeUYUAAAAADtuawZIutfIUO5U8badCga1I5yL"></div>
-                        <input type="submit" id="btn-footer-submit" value="Send Message" style="margin-top: 0px;" />
-                    </form>
-                    <div class="fb">
-                        <a href="http://line.me/ti/p/~ALUMINATION" target="_blank">
-                            <img src="<?php echo base_url('storage/images/line-icon.png'); ?>" />
-                        </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <a href="https://www.facebook.com/ALUINCH/" target="_blank"><img src="<?php echo base_url('storage/images/fb2.png'); ?>" /></a>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div class="copy mobile">Sitemap | Terms and Conditions</div>
-                    <div class="copy mobile">Copyright © 2015 www.aluinch.com All Rights Reserved. SEO maintained by iSEO.in.th</div>
-                    <div class="clearfix"></div>
-                    <div class="copy-line mobile"></div>
-                    <div class="partner mobile">partnership by <span class="yellow">In The Glazz</span> Company</div>
-                </div>
-            </div>
-            <div class="clearfix"></div>
-        </div>
-        <div id="map-canvas" class="map-google"></div>
-    </div>
+    <?php include 'layouts/contact.php'; ?>
     <!-- end contact -->
-
-    <!-- JS Libraies -->
-
-    <!-- Page Specific JS File -->
 
     <!-- Push Custom Scripts -->
     <script>
         $(function(){
-            $("#navi ul li.technology-nav").addClass("active");
-
-
             size_li = $("#list-loadmore li").size();
             x=6;
             $('#list-loadmore li:lt('+x+')').show();
